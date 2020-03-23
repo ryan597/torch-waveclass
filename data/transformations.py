e15503f78fabb0dac9f6fb57deb6129e299a21ce
Ryan Smith <ryan.smith@ucdconnect.ie>
2020-03-23 19:25:12 +0000

    move normalization inside transform

--- data/transformations.py
+++ data/transformations.py
@@ -4,42 +4,45 @@ the ToPILImage() transform is commented out"""
 import torchvision.transforms as T
 
 
-def get_normalization(base):
-    """"Return the appropriate normalisation parameters for the base CNN,
-    in the form [mean[R,G,B], std[R,G,B]]."""
-    pretrain_transform = {
-        #"resnet18" :   [[,,], [,,]],
-        #"alexnet" :    [[,,], [,,]],
-        "vgg16" :      [[0.5, 0.5, 0.5], [0.5, 0.5, 0.5]],
-        #"squeezenet" : [[,,], [,,]],
-        #"densenet" :   [[,,], [,,]],
-        #"inception" :  [[,,], [,,]],
-        #"googlenet" :  [[,,], [,,]],
-        "mobilenet" :  [[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]]
-    }
-    return pretrain_transform[base]
 
-def get_transform(base, augment=False, image_shape=(224, 224)):
+# DEPRECATED : All pytorch pretrained models use the same normalisation
+# See https://github.com/pytorch/examples/blob/97304e232807082c2e7b54c597615dc0ad8f6173/imagenet/main.py#L197-L198
+#def get_normalization(base):
+#    """"Return the appropriate normalisation parameters for the base CNN,
+#    in the form [mean[R,G,B], std[R,G,B]]."""
+#    pretrain_transform = {
+#        #"resnet18" :   [[0.485, 0.456, 0.406], [0.299, 0.244, 0.255]],
+#        #"alexnet" :    [[,,], [,,]],
+#        "vgg16" :      [[0.5, 0.5, 0.5], [0.5, 0.5, 0.5]],
+#        #"squeezenet" : [[,,], [,,]],
+#        #"densenet" :   [[,,], [,,]],
+#        #"inception" :  [[,,], [,,]],
+#        #"googlenet" :  [[,,], [,,]],
+#        "mobilenet" :  [[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]]
+#    }
+#    return pretrain_transform[base]
+
+
+
+def get_transform(augment=False, image_shape=(224, 224)):
     """Return the transformations/augmentations for a given base CNN model."""
-    mean, std = get_normalization(base)
 
     if augment:
-        transform = T.Compose([
+        preprocess_transform = T.Compose([
             T.ToPILImage(),
             T.RandomRotation(degrees=15, fill=(0,)),
-            T.RandomResizedCrop(size=image_shape, scale=(0.8, 1.0)),
-            T.Grayscale(num_output_channels=3),
-            T.ToTensor(),
-            T.Normalize(mean, std)
+            T.RandomResizedCrop(size=image_shape, scale=(0.8, 1.0))
         ])
-
     else:
-        transform = T.Compose([
+        preprocess_transform = T.Compose([
             T.ToPILImage(),
-            T.Resize(size=image_shape),
-            T.Grayscale(num_output_channels=3),
-            T.ToTensor(),
-            T.Normalize(mean, std)
+            T.Resize(size=image_shape)
         ])
 
-    return transform
+    pretrain_transform = T.Compose([
+        T.Grayscale(num_output_channels=3),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    return T.Compose([preprocess_transform, pretrain_transform])
